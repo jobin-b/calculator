3f5e9cfb284bce633c3a46d773843f67e0b721a5
Jobin <120872413+jobin-b@users.noreply.github.com>
2023-02-14 18:32:06 -0500

    working calculator

--- script.js
+++ script.js
@@ -30,3 +30,82 @@ function operate(op, a, b){ //takes operator and two numbers
     }
 }
 
+let x = 0, y = 0, fn;
+let xInput = false;
+let yInput = false;
+let state = 0;
+let nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8" ,"9"];
+let ops = ["+", "-", "/", "*"];
+screen = document.getElementById("screen");
+
+
+function buffer(btn){
+    let input = btn.innerText;
+    if(nums.includes(input)){
+        if(state === 2){
+            state = 0;
+            x = 0;
+        }
+        if(state === 0){
+            x = x * 10 + +input;
+            screen.innerText = x;
+            xInput = true;
+        } else {
+            y = y * 10 + +input;
+            screen.innerText = y;
+            yInput = true;
+        }
+    } else if(ops.includes(input)){
+        if(xInput && !yInput){
+            fn = input;
+            state = 1;
+        } else if(yInput){
+            x = operate(fn, x, y)
+            x = rnd(x);
+            screen.innerText = x;
+            yInput = false;
+            y = 0;
+            state = 1;
+            fn = input;
+        }
+    } else if(input === "="){
+        if(!yInput){
+            screen.innerText = "ERROR: INVALID INPUT";
+        }else if(fn === "/" && y === 0){
+            screen.innerText = "ERROR: DIVIDE BY ZERO";
+            x = 0;
+            y = 0;
+            xInput = false;
+            yInput = false;
+            state = 0;
+        } else {
+            x = operate(fn, x, y);
+            x = rnd(x);
+            screen.innerText = x;
+            y = 0;
+            yInput = false;
+            state = 2; //special state where either uses answer or writes new operation
+        }
+        
+    }
+    if(input === "Clear") {
+        x = 0;
+        y = 0;
+        xInput = false;
+        yInput = false;
+        state = 0;
+        screen.innerText = "";
+    }
+    
+}
+
+function rnd(dec){
+    return Math.round(dec*1000000)/1000000;
+}
+
+buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", function(){
+        buffer(button);
+    });
+});
